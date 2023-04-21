c89de42b04dd73c8db41877fdb9bb96b5a82d008
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-04-21 15:28:24 +0700

    add level column

--- database/migrations/2023_04_06_035638_create_tasks_table.php
+++ database/migrations/2023_04_06_035638_create_tasks_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            // $table->date("deadline");
             $table->dateTime('deadline');
             $table->integer("level");
             $table->string('description');
